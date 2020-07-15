ca285952bace53c7256b3ab17104d886586dedd5
Rafael Santana <rafaelsantana7213@gmail.com>
2020-07-15 18:05:13 -0300

    Updates

---
+++ precache-manifest.e35bfadd1cac9726da9fc8b525e9261e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda091a2d8b14bcb5fb1bbf72da8da35",
+    "url": "/cronometroReact/index.html"
+  },
+  {
+    "revision": "9a62b2530483feb7b2bd",
+    "url": "/cronometroReact/static/css/main.ff6bf40b.chunk.css"
+  },
+  {
+    "revision": "62e233f6c81e36c43a37",
+    "url": "/cronometroReact/static/js/2.918086c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cronometroReact/static/js/2.918086c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a62b2530483feb7b2bd",
+    "url": "/cronometroReact/static/js/main.381de971.chunk.js"
+  },
+  {
+    "revision": "d1edb8ab70897b8ca69e",
+    "url": "/cronometroReact/static/js/runtime-main.5617b438.js"
+  },
+  {
+    "revision": "54825791bba9807d5e4975c702d4d4d0",
+    "url": "/cronometroReact/static/media/cronometro.54825791.png"
+  }
+]);
